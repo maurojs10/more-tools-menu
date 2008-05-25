e1050ea6c06d2cd90d539011fe941ac6f1c09414
Anthony Lieuallen <arantius@gmail.com>
2008-05-25 22:10:35 +0000

    -

--- content/moretools.js
+++ content/moretools.js
@@ -1,59 +1,48 @@
 (function() {
-var orgDocument=document.lastChild.cloneNode(true);
-orgDocument.getElementById=document.getElementById;
+
+var insertEvents=[];
+
+function catchInsertEvent(event) {
+	// just cache the event.  if we try to access the object, we screw things up 
+	insertEvents[insertEvents.length]=event;
+}
 
 function mungeMenus(event) {
+	document.removeEventListener('DOMNodeInserted', catchInsertEvent, true);
 	window.removeEventListener('DOMContentLoaded', mungeMenus, true);
 
 	var toolsMenu=
 		document.getElementById('menu_ToolsPopup') || // firefox
 		document.getElementById('taskPopup') ;        // thunderbird
+
 	var moreToolsMenu=document.getElementById('more-tools-menupopup');
 
-	var orgMenuPopups=orgDocument.getElementsByTagName("menupopup");
-	var orgToolsMenu=null;
-	for (var i=0, popup=null; popup=orgMenuPopups[i]; i++) {
-		if (popup.id=="menu_ToolsPopup" || // firefox
-		    popup.id=="taskPopup"          // thunderbird
-		) {
-			orgToolsMenu=orgMenuPopups[i];
-		}
-	}
-
 	var mungeFlag=false;
-	var moveTool=function(e) {
-		if (e.target) e=e.target;
 
-		toolsMenu.removeChild(e);
-		moreToolsMenu.appendChild(e);
+	// for each insert event, find the element, and decide
+	// if we should do something with it
+	for (var i=insertEvents.length-1, event=null, el=null; event=insertEvents[i]; i--) {
+		try {
+			el=event.target;
 
-		if (!mungeFlag) {
-			document.getElementById('more-tools-label').setAttribute('hidden', true);
-			document.getElementById('more-tools-sep').setAttribute('hidden', true);
-		}
-		mungeFlag=true;
+			if (toolsMenu!=el.parentNode) continue;
+			// if we got here, the insert was to the tools menu.  move the element!
+			toolsMenu.removeChild(el);
+			moreToolsMenu.appendChild(el);
+
+			mungeFlag=true;
+		} catch (e) { }
 	}
 
-	function nodeWasInOrgMenu(node) {
-		for (var i=0, orgNode=false; orgNode=orgToolsMenu.childNodes[i]; i++) {
-			if (node.isEqualNode(orgNode)) return true;
-		}
-		return false;
+	if (mungeFlag) {
+		// we did munge something into this menu; remove the label and separator
+		document.getElementById('more-tools-label').setAttribute('hidden', true);
+		document.getElementById('more-tools-sep').setAttribute('hidden', true);
 	}
-
-	for (var i=0, node=false; node=toolsMenu.childNodes[i]; i++) {
-		if (!nodeWasInOrgMenu(node)) {
-			moveTool(node);
-		}
-	}
-
-	// Free this memory.
-	orgDocument=null;
-	orgToolsMenu=null;
-	orgMenuPopups=null;
-
-	// Move any node added in the future.
-	toolsMenu.addEventListener('DOMNodeInserted', moveTool, true)
 }
+
+document.addEventListener('DOMNodeInserted', catchInsertEvent, true);
 window.addEventListener('DOMContentLoaded', mungeMenus, true);
+
+
 })();
