7356e40816a01a6709645635acff19ab7dd1354a
Anthony Lieuallen <arantius@gmail.com>
2008-05-25 18:40:25 +0000

    * Drop new FF3 method; it's unreliable.

--- content/moretools.js
+++ content/moretools.js
@@ -1,39 +1,48 @@
 (function() {
-// At initial run, find and mark all the elements in the tools menu.
-var toolsMenu=document.getElementById('menu_ToolsPopup');
-var tools=toolsMenu.childNodes;
-for (var i=0, t; t=tools[i]; i++) {
-	t.setAttribute('moreToolsMark', '');
+
+var insertEvents=[];
+
+function catchInsertEvent(event) {
+	// just cache the event.  if we try to access the object, we screw things up 
+	insertEvents[insertEvents.length]=event;
 }
 
-// Then, after the window has loaded, find and move any un-marked items.
-function moveInitialTools() {
-	window.removeEventListener('load', moveInitialTools, true);
+function mungeMenus(event) {
+	document.removeEventListener('DOMNodeInserted', catchInsertEvent, true);
+	window.removeEventListener('DOMContentLoaded', mungeMenus, true);
+
+	var toolsMenu=
+		document.getElementById('menu_ToolsPopup') || // firefox
+		document.getElementById('taskPopup') ;        // thunderbird
 
 	var moreToolsMenu=document.getElementById('more-tools-menupopup');
-	var tools=document.getElementById('menu_ToolsPopup').childNodes;
+
 	var mungeFlag=false;
-	
-	var moveTool=function(e) {
-		if (e.target) e=e.target;
 
-		toolsMenu.removeChild(e);
-		moreToolsMenu.appendChild(e);		
-		
-		if (!mungeFlag) {
-			document.getElementById('more-tools-label')
-				.setAttribute('hidden', true);
-		}
-		mungeFlag=true;
+	// for each insert event, find the element, and decide
+	// if we should do something with it
+	for (var i=insertEvents.length-1, event=null, el=null; event=insertEvents[i]; i--) {
+		try {
+			el=event.target;
+
+			if (toolsMenu!=el.parentNode) continue;
+			// if we got here, the insert was to the tools menu.  move the element!
+			toolsMenu.removeChild(el);
+			moreToolsMenu.appendChild(el);
+
+			mungeFlag=true;
+		} catch (e) { }
 	}
 
-	for (var i=0, t; t=tools[i]; i++) {
-		if (t.hasAttribute('moreToolsMark')) continue;
-		moveTool(t);
+	if (mungeFlag) {
+		// we did munge something into this menu; remove the label and separator
+		document.getElementById('more-tools-label').setAttribute('hidden', true);
+		document.getElementById('more-tools-sep').setAttribute('hidden', true);
 	}
-
-	// And move anything added later.
-	toolsMenu.addEventListener('DOMNodeInserted', moveTool, true);
 }
-window.addEventListener('load', moveInitialTools, true);
+
+document.addEventListener('DOMNodeInserted', catchInsertEvent, true);
+window.addEventListener('DOMContentLoaded', mungeMenus, true);
+
+
 })();
