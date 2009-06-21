3ba8dcbf95bbce13c0c4d8a3a3913ac8e9a8e785
Anthony Lieuallen <arantius@gmail.com>
2009-06-21 00:41:07 +0000

    Fixes #57, #117, #202, #255, #284
     * Update compatibility flag w/ Firefox 3.5.
     * Rewrite the code to move menu items for Firefox, which catches all extensions mentioned in users' tickets.

--- content/moretools-ff3.js
+++ content/moretools-ff3.js
@@ -1,68 +1,42 @@
 (function() {
 
-// duplicate original window (drastic, but anything else either prevents node insertion or throws an exception)
-var originalWindow = document.lastChild.cloneNode(true);
+var toolsMenuPopup=
+	document.getElementById('menu_ToolsPopup') || // firefox
+	document.getElementById('taskPopup');         // thunderbird
+var moreToolsMenuPopup;
+var mungeFlag;
 
-function mungeMenus(event) {
-	window.removeEventListener('DOMContentLoaded', mungeMenus, true);
-
-	var toolsMenu=
-		document.getElementById('menu_ToolsPopup') || // firefox
-		document.getElementById('taskPopup') ;        // thunderbird
-
-	var moreToolsMenu=document.getElementById('more-tools-menupopup');
-
-	// Find the original tools menu popup
-	var originalMenuPopups = originalWindow.getElementsByTagName("menupopup");
-	var originalToolsMenu=null;
-	for (i=0; i<originalMenuPopups.length; i++) {
-		if (originalMenuPopups[i].id == "menu_ToolsPopup" || // firefox
-		    originalMenuPopups[i].id == "taskPopup") {       // thunderbird
-			originalToolsMenu = originalMenuPopups[i];
-		}
-	}
-	originalMenuPopups = null;
-		
-	var mungeFlag=false;
-	
-	// function to move menu item from tools menu to more tools menu
-	var moveElem = function(el) {
-		toolsMenu.removeChild(el);
-		moreToolsMenu.appendChild(el);
-		
-		mungeFlag=true;
-	}
-	
-	// loop through the original menu toolbar and the current menu toolbar.  Move anything
-	// that's in the current one that wasn't there originally
-	var i=0;
-	for (j=0; j<originalToolsMenu.childNodes.length; j++) {
-		while ((originalToolsMenu.childNodes[j].id != toolsMenu.childNodes[i].id) &&
-				 (originalToolsMenu.childNodes[j].id != toolsMenu.childNodes[i].id)) {
-		
-			moveElem(toolsMenu.childNodes[i]);
-		}
-		i++;
-	}
-	
-	// Move remaining menu items from tools menu
-	while (i < toolsMenu.childNodes.length) {
-		moveElem(toolsMenu.childNodes[i]);
-	}		
-   
-	// don't need cloned window any more so get rid of it 
-	originalToolsMenu = null;
-	originalWindow = null;
-
-	if (mungeFlag) {
-		// we did munge something into this menu; remove the label and separator
-		document.getElementById('more-tools-label').setAttribute('hidden', true);
-		document.getElementById('more-tools-sep').setAttribute('hidden', true);
-	}
+function moveTool(el) {
+	moreToolsMenuPopup.appendChild(el);
+	mungeFlag=true;
 }
 
+function moveAllTools() {
+	var mungeFlag=false;
+	for (var i=0, el=null; el=toolsMenuPopup.childNodes[i]; i++) {
+		if (!el.hasAttribute('nativeTool')) {
+			moveTool(el);
+		}
+	}
+	if (mungeFlag) hideDescription();
+}
 
-window.addEventListener('DOMContentLoaded', mungeMenus, true);
+function hideDescription() {
+	document.getElementById('more-tools-label').setAttribute('hidden', true);
+}
 
+// Mark all tools items that are already here.
+for (var i=0, el=null; el=toolsMenuPopup.childNodes[i]; i++) {
+	el.setAttribute('nativeTool', 'true');
+}
+
+window.addEventListener('load', function() {
+	// At load time, find the more-tools menu.
+	moreToolsMenuPopup=document.getElementById('more-tools-menupopup');
+	// Move any non-marked tools that are already present.
+	moveAllTools();
+	// Move any non-marked tool that are added later.
+	toolsMenuPopup.addEventListener('DOMNodeInserted', moveAllTools, true);
+}, true);
 
 })();
