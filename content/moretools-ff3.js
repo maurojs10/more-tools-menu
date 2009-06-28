180fbe451fdd889eb9b413135db5fdccebf9f7ea
Anthony Lieuallen <arantius@gmail.com>
2009-06-28 23:32:46 +0000

    Fixes #290
     * Rewrite tools moving logic again, this time taking compatibility with toolbar customization.

--- content/moretools-ff3.js
+++ content/moretools-ff3.js
@@ -1,40 +1,72 @@
 (function() {
 
-var toolsMenuPopup=
-	document.getElementById('menu_ToolsPopup') || // firefox
-	document.getElementById('taskPopup');         // thunderbird
+var toolsMenuPopup;
 var moreToolsMenuPopup;
-var mungeFlag;
+var origTools=getTools(document);
 
-function moveTool(el) {
-	moreToolsMenuPopup.appendChild(el);
-	mungeFlag=true;
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+
+function getTools(node) {
+	function nsResolver() {
+		return 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
+	}
+
+	var doc=node.ownerDocument || node;
+
+	var nodesSnapshot=doc.evaluate(
+		"//xul:menupopup[@id='menu_ToolsPopup' or @id='taskPopup']/*",
+		doc, nsResolver, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null
+	);
+
+	return nodesSnapshot;
+
+	var result=[];
+	for (var i=0; i<nodesSnapshot.snapshotLength; i++) {
+		result.push( nodesSnapshot.snapshotItem(i) );
+	}
+
+	return result;
 }
 
 function moveAllTools() {
 	var mungeFlag=false;
-	for (var i=0, el=null; el=toolsMenuPopup.childNodes[i]; i++) {
+	var currTools=getTools(document);
+	for (var i=0; i<currTools.snapshotLength; i++) {
+		var el=currTools.snapshotItem(i);
 		if (!el.hasAttribute('nativeTool')) {
-			moveTool(el);
+			if ('menuseparator'==el.tagName) {
+				toolsMenuPopup.removeChild(el);
+			} else {
+				moreToolsMenuPopup.appendChild(el);
+			}
+			mungeFlag=true;
 		}
 	}
-	if (mungeFlag) hideDescription();
+	if (mungeFlag) {
+		document.getElementById('more-tools-label').setAttribute('hidden', true);
+		document.getElementById('more-tools-sep').setAttribute('hidden', true);
+	}
 }
 
-function hideDescription() {
-	document.getElementById('more-tools-label').setAttribute('hidden', true);
-}
-
-// Mark all tools items that are already here.
-for (var i=0, el=null; el=toolsMenuPopup.childNodes[i]; i++) {
-	el.setAttribute('nativeTool', 'true');
-}
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
 window.addEventListener('load', function() {
-	// At load time, find the more-tools menu.
+	// Look up nodes now that getElementById() is safe.
+	toolsMenuPopup=
+		document.getElementById('menu_ToolsPopup') || // firefox
+		document.getElementById('taskPopup');         // thunderbird
 	moreToolsMenuPopup=document.getElementById('more-tools-menupopup');
+
+	// Mark all tools items that were originally here.
+	for (var i=0; i<origTools.snapshotLength; i++) {
+		var el=origTools.snapshotItem(i);
+		el.setAttribute('nativeTool', 'true');
+	}
+	origTools=null;
+
 	// Move any non-marked tools that are already present.
 	moveAllTools();
+
 	// Move any non-marked tool that are added later.
 	toolsMenuPopup.addEventListener('DOMNodeInserted', moveAllTools, true);
 }, true);
