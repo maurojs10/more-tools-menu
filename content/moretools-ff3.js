28e7ec09c9595f3d495e9caba33bda419ab8df9b
Anthony Lieuallen <arantius@gmail.com>
2011-06-26 21:19:47 +0000

    giant style refactor

--- content/moretools-ff3.js
+++ content/moretools-ff3.js
@@ -7,68 +7,68 @@ var origTools=getTools(document);
 // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
 function getTools(node) {
-	function nsResolver() {
-		return 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
-	}
+  function nsResolver() {
+    return 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
+  }
 
-	var doc=node.ownerDocument || node;
+  var doc=node.ownerDocument || node;
 
-	var nodesSnapshot=doc.evaluate(
-		"//xul:menupopup[@id='menu_ToolsPopup' or @id='taskPopup']/*",
-		doc, nsResolver, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null
-	);
+  var nodesSnapshot=doc.evaluate(
+    "//xul:menupopup[@id='menu_ToolsPopup' or @id='taskPopup']/*",
+    doc, nsResolver, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null
+  );
 
-	return nodesSnapshot;
+  return nodesSnapshot;
 
-	var result=[];
-	for (var i=0; i<nodesSnapshot.snapshotLength; i++) {
-		result.push( nodesSnapshot.snapshotItem(i) );
-	}
+  var result=[];
+  for (var i=0; i<nodesSnapshot.snapshotLength; i++) {
+    result.push( nodesSnapshot.snapshotItem(i) );
+  }
 
-	return result;
+  return result;
 }
 
 function moveAllTools() {
-	var mungeFlag=false;
-	var currTools=getTools(document);
-	for (var i=0; i<currTools.snapshotLength; i++) {
-		var el=currTools.snapshotItem(i);
-		if (!el.hasAttribute('nativeTool')) {
-			if ('menuseparator'==el.tagName) {
-				toolsMenuPopup.removeChild(el);
-			} else {
-				moreToolsMenuPopup.appendChild(el);
-			}
-			mungeFlag=true;
-		}
-	}
-	if (mungeFlag) {
-		document.getElementById('more-tools-label').setAttribute('hidden', true);
-		document.getElementById('more-tools-sep').setAttribute('hidden', true);
-	}
+  var mungeFlag=false;
+  var currTools=getTools(document);
+  for (var i=0; i<currTools.snapshotLength; i++) {
+    var el=currTools.snapshotItem(i);
+    if (!el.hasAttribute('nativeTool')) {
+      if ('menuseparator'==el.tagName) {
+        toolsMenuPopup.removeChild(el);
+      } else {
+        moreToolsMenuPopup.appendChild(el);
+      }
+      mungeFlag=true;
+    }
+  }
+  if (mungeFlag) {
+    document.getElementById('more-tools-label').setAttribute('hidden', true);
+    document.getElementById('more-tools-sep').setAttribute('hidden', true);
+  }
 }
 
 // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
 
 window.addEventListener('load', function() {
-	// Look up nodes now that getElementById() is safe.
-	toolsMenuPopup=
-		document.getElementById('menu_ToolsPopup') || // firefox
-		document.getElementById('taskPopup');         // thunderbird
-	moreToolsMenuPopup=document.getElementById('more-tools-menupopup');
+  // Look up nodes now that getElementById() is safe.
+  toolsMenuPopup=
+    document.getElementById('menu_ToolsPopup') || // firefox
+    document.getElementById('taskPopup');         // thunderbird
+  moreToolsMenuPopup=document.getElementById('more-tools-menupopup');
 
-	// Mark all tools items that were originally here.
-	for (var i=0; i<origTools.snapshotLength; i++) {
-		var el=origTools.snapshotItem(i);
-		el.setAttribute('nativeTool', 'true');
-	}
-	origTools=null;
+  // Mark all tools items that were originally here.
+  for (var i=0; i<origTools.snapshotLength; i++) {
+    var el=origTools.snapshotItem(i);
+    el.setAttribute('nativeTool', 'true');
+  }
+  origTools=null;
 
-	// Move any non-marked tools that are already present.
-	moveAllTools();
+  // Move any non-marked tools that are already present.
+  moveAllTools();
 
-	// Move any non-marked tool that are added later.
-	toolsMenuPopup.addEventListener('DOMNodeInserted', moveAllTools, true);
+  // Move any non-marked tool that are added later.
+  toolsMenuPopup.addEventListener('DOMNodeInserted', moveAllTools, true);
 }, true);
 
 })();
--- content/moretools.js
+++ content/moretools.js
@@ -3,46 +3,45 @@
 var insertEvents=[];
 
 function catchInsertEvent(event) {
-	// just cache the event.  if we try to access the object, we screw things up 
-	insertEvents[insertEvents.length]=event;
+  // just cache the event.  if we try to access the object, we screw things up
+  insertEvents[insertEvents.length]=event;
 }
 
 function mungeMenus(event) {
-	document.removeEventListener('DOMNodeInserted', catchInsertEvent, true);
-	window.removeEventListener('DOMContentLoaded', mungeMenus, true);
+  document.removeEventListener('DOMNodeInserted', catchInsertEvent, true);
+  window.removeEventListener('DOMContentLoaded', mungeMenus, true);
 
-	var toolsMenu=
-		document.getElementById('menu_ToolsPopup') || // firefox
-		document.getElementById('taskPopup') ;        // thunderbird
+  var toolsMenu=
+      document.getElementById('menu_ToolsPopup') || // firefox
+      document.getElementById('taskPopup') ;        // thunderbird
 
-	var moreToolsMenu=document.getElementById('more-tools-menupopup');
+  var moreToolsMenu=document.getElementById('more-tools-menupopup');
 
-	var mungeFlag=false;
+  var mungeFlag=false;
 
-	// for each insert event, find the element, and decide
-	// if we should do something with it
-	for (var i=insertEvents.length-1, event=null, el=null; event=insertEvents[i]; i--) {
-		try {
-			el=event.target;
+  // for each insert event, find the element, and decide
+  // if we should do something with it
+  for (var i=insertEvents.length-1, event=null, el=null; event=insertEvents[i]; i--) {
+    try {
+      el=event.target;
 
-			if (toolsMenu!=el.parentNode) continue;
-			// if we got here, the insert was to the tools menu.  move the element!
-			toolsMenu.removeChild(el);
-			moreToolsMenu.appendChild(el);
+      if (toolsMenu!=el.parentNode) continue;
+      // if we got here, the insert was to the tools menu.  move the element!
+      toolsMenu.removeChild(el);
+      moreToolsMenu.appendChild(el);
 
-			mungeFlag=true;
-		} catch (e) { }
-	}
+      mungeFlag=true;
+    } catch (e) { }
+  }
 
-	if (mungeFlag) {
-		// we did munge something into this menu; remove the label and separator
-		document.getElementById('more-tools-label').setAttribute('hidden', true);
-		document.getElementById('more-tools-sep').setAttribute('hidden', true);
-	}
+  if (mungeFlag) {
+    // we did munge something into this menu; remove the label and separator
+    document.getElementById('more-tools-label').setAttribute('hidden', true);
+    document.getElementById('more-tools-sep').setAttribute('hidden', true);
+  }
 }
 
 document.addEventListener('DOMNodeInserted', catchInsertEvent, true);
 window.addEventListener('DOMContentLoaded', mungeMenus, true);
-
 
 })();
