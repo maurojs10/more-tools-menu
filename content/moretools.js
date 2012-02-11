ebc9a03345b3d975a5841913060ed1b075b8e6e0
maurojs10 <maurojs10@gmail.com>
2012-02-10 23:57:05 -0200

    Code rewrite to be compatible with SeaMonkey and to allow user to choose which items to keep in Tools menu and which items to move to More Tools menu.

--- content/moretools.js
+++ content/moretools.js
@@ -1,94 +1,160 @@
-(function() {
+var moreTools = {
+	toolsMenuPopup: undefined,
+	moreToolsMenuPopup: undefined,
+	itemsToKeep: undefined,
 
-var toolsMenuPopup;
-var moreToolsMenuPopup;
-var origTools=getTools(document);
-var nativeToolsNotFound = false;
+	applicationName: '',
+	documentName: '',
 
-// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+	prefs: {
+		showWhenEmpty: { name: 'showWhenEmpty', value: false },
+		extensionToolsToKeep: { name: 'extensionToolsToKeep', value: ''},
+		nativeToolsToMove: { name: 'nativeToolsToMove', value: ''},
+		nativeTools: { name: 'nativeTools', value: ''}
+	},
 
-function getTools(node) {
-  function nsResolver() {
-    return 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul';
-  }
+	loadPrefs: function () {
+		'use strict';
 
-  var doc=node.ownerDocument || node;
+		var prefBranch;
 
-  var nodesSnapshot=doc.evaluate(
-    "//xul:menupopup[@id='menu_ToolsPopup' or @id='taskPopup']/*",
-    doc, nsResolver, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null
-  );
+		prefBranch = Components.classes['@mozilla.org/preferences-service;1'].getService(Components.interfaces.nsIPrefService).getBranch('extensions.moretools.');
+		this.prefs.showWhenEmpty.value = prefBranch.getBoolPref(this.prefs.showWhenEmpty.name);
+		this.prefs.extensionToolsToKeep.value = prefBranch.getCharPref(this.prefs.extensionToolsToKeep.name);
+		this.prefs.nativeToolsToMove.value = prefBranch.getCharPref(this.prefs.nativeToolsToMove.name);
+		this.prefs.nativeTools.value = prefBranch.getCharPref(this.prefs.nativeTools.name + '.' + this.applicationName + '.' + this.documentName);
+	},
 
-  // Awful workaround for Thunderbird
-  if (nativeToolsNotFound) {
-    /* Since we couldn't detect the native tools on window load, let's hardcode their ids here.
-     * References:
-     *   http://mxr.mozilla.org/mozilla/source/mail/base/content/mailWindowOverlay.xul
-     *   http://mxr.mozilla.org/comm-central/source/mail/base/content/mailWindowOverlay.xul
-     * */
-    var taskPopupItems = [ 'tasksMenuMail', 'addressBook', 'devToolsSeparator', 'menu_openSavedFilesWnd', 'addonsManager', 'activityManager', 'filtersCmd', 'applyFilters', 'applyFiltersToSelection', 'tasksMenuAfterApplySeparator', 'runJunkControls', 'deleteJunk', 'tasksMenuAfterDeleteSeparator', 'menu_import', 'javaScriptConsole', 'javascriptConsole', 'prefSep', 'menu_accountmgr', 'menu_preferences', 'menu_mac_services', 'menu_mac_hide_app', 'menu_mac_hide_others', 'menu_mac_show_all' ];
+	getToolsMenuPopupSnapshot: function () {
+		'use strict';
 
-    for (var i = 0, l = nodesSnapshot.snapshotLength; i < l; i++)
-      for (var j = 0, k = taskPopupItems.length; j < k; j++)
-        if (nodesSnapshot.snapshotItem(i).id == taskPopupItems[j]) {
-          nodesSnapshot.snapshotItem(i).setAttribute('nativeTool', 'true');
-          break;
-        }
-  }
+		var doc = document.ownerDocument || document;
+		return doc.evaluate(
+			'//xul:menupopup[@id="menu_ToolsPopup" or @id="taskPopup"]/*',
+			doc,
+			function () { return 'http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul'; },
+			XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+			null
+		);
+	},
 
-  return nodesSnapshot;
+	updateItemsToKeep: function () {
+		'use strict';
 
-  var result=[];
-  for (var i=0; i<nodesSnapshot.snapshotLength; i++) {
-    result.push( nodesSnapshot.snapshotItem(i) );
-  }
+		var itemsToKeepArray = this.prefs.nativeTools.value.replace(/\s*,\s*/g, ',').split(',');
+		if (this.prefs.nativeToolsToMove.value.trim() !== '') {
+			itemsToKeepArray = itemsToKeepArray.diff(this.prefs.nativeToolsToMove.value.replace(/\s*,\s*/g, ',').split(','));
+		}
+		if (this.prefs.extensionToolsToKeep.value.trim() !== '') {
+			itemsToKeepArray = itemsToKeepArray.union(this.prefs.extensionToolsToKeep.value.replace(/\s*,\s*/g, ',').split(','));
+		}
+		this.itemsToKeep = new RegExp('^(' + itemsToKeepArray.join('|') + ')$');
+	},
 
-  return result;
+	dumpToolsMenuItems: function () {
+		'use strict';
+
+		var toolsMenuPopupSnapshot, i, l, message1, message2;
+
+		toolsMenuPopupSnapshot = this.getToolsMenuPopupSnapshot();
+		if (toolsMenuPopupSnapshot) {
+			message1 = 'Items found in Tools menu at startup (' + toolsMenuPopupSnapshot.snapshotLength + '):\n';
+			message2 = "pref('extensions.moretools.nativeTools." + this.applicationName + "." + this.documentName + "', '";
+			for (i = 0, l = toolsMenuPopupSnapshot.snapshotLength; i < l; i += 1) {
+				message1 += '\t' + toolsMenuPopupSnapshot.snapshotItem(i).id + '\t' + toolsMenuPopupSnapshot.snapshotItem(i).label + '\n';
+				if (toolsMenuPopupSnapshot.snapshotItem(i).id !== '') {
+					message2 += toolsMenuPopupSnapshot.snapshotItem(i).id + ', ';
+				}
+			}
+			if (message2.substring(message2.length - 2) === ', ') {
+				message2 = message2.slice(0, message2.length - 2);
+			}
+			message2 += "');";
+			Components.classes['@mozilla.org/consoleservice;1'].getService(Components.interfaces.nsIConsoleService).logStringMessage(message1);
+			Components.classes['@mozilla.org/consoleservice;1'].getService(Components.interfaces.nsIConsoleService).logStringMessage(message2);
+		}
+	},
+
+	moveTools: function () {
+		'use strict';
+
+		var toolsMenuPopupSnapshot, i, l, item, moreToolsMenuIsEmpty;
+
+		toolsMenuPopupSnapshot = this.getToolsMenuPopupSnapshot();
+		if (toolsMenuPopupSnapshot) {
+			for (i = 0, l = toolsMenuPopupSnapshot.snapshotLength; i < l; i += 1) {
+				item = toolsMenuPopupSnapshot.snapshotItem(i);
+				if (item && item.id && !this.itemsToKeep.test(item.id)) {
+					if (item.tagName === 'menuseparator') {
+						this.toolsMenuPopup.removeChild(item);
+					} else {
+						this.moreToolsMenuPopup.appendChild(item);
+					}
+				}
+			}
+		}
+
+		moreToolsMenuIsEmpty = (this.moreToolsMenuPopup.childNodes.length === 2);
+
+		if (moreToolsMenuIsEmpty) {
+			document.getElementById('more-tools-label').setAttribute('hidden', false);
+			document.getElementById('more-tools-sep').setAttribute('hidden', false);
+		} else {
+			document.getElementById('more-tools-label').setAttribute('hidden', true);
+			document.getElementById('more-tools-sep').setAttribute('hidden', true);
+		}
+
+		if (moreToolsMenuIsEmpty && this.prefs.showWhenEmpty.value === false) {
+			document.getElementById('more-tools-menu').setAttribute('hidden', true);
+		} else {
+			document.getElementById('more-tools-menu').setAttribute('hidden', false);
+		}
+	},
+
+	startup: function () {
+		'use strict';
+
+		this.applicationName = Components.classes['@mozilla.org/xre/app-info;1'].getService(Components.interfaces.nsIXULAppInfo).name.toLowerCase();
+		this.documentName = document.documentURI.replace(/^chrome:\/\/[a-z\/]+\/([a-z]+)\.xul$/i, '$1');
+
+		this.loadPrefs();
+
+		this.toolsMenuPopup =
+			document.getElementById('menu_ToolsPopup') ||  // Firefox
+			document.getElementById('taskPopup');          // Thunderbird, SeaMonkey
+		this.moreToolsMenuPopup = document.getElementById('more-tools-menupopup');
+
+		this.dumpToolsMenuItems();
+
+		this.updateItemsToKeep();
+
+		this.moveTools();
+
+		this.toolsMenuPopup.addEventListener('DOMNodeInserted', moreTools_moveTools, true);
+	},
+
+	shutdown: function () {
+		'use strict';
+	}
+};
+
+function moreTools_startup() {
+	'use strict';
+
+	moreTools.startup();
 }
 
-function moveAllTools() {
-  var mungeFlag=false;
-  var currTools=getTools(document);
-  for (var i=0; i<currTools.snapshotLength; i++) {
-    var el=currTools.snapshotItem(i);
-    if (!el.hasAttribute('nativeTool')) {
-      if ('menuseparator'==el.tagName) {
-        toolsMenuPopup.removeChild(el);
-      } else {
-        moreToolsMenuPopup.appendChild(el);
-      }
-      mungeFlag=true;
-    }
-  }
-  if (mungeFlag) {
-    document.getElementById('more-tools-label').setAttribute('hidden', true);
-    document.getElementById('more-tools-sep').setAttribute('hidden', true);
-  }
+function moreTools_shutdown() {
+	'use strict';
+
+	moreTools.shutdown();
 }
 
-// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+function moreTools_moveTools() {
+	'use strict';
 
-window.addEventListener('load', function() {
-  // Look up nodes now that getElementById() is safe.
-  toolsMenuPopup=
-    document.getElementById('menu_ToolsPopup') || // firefox
-    document.getElementById('taskPopup');         // thunderbird
-  moreToolsMenuPopup=document.getElementById('more-tools-menupopup');
+	moreTools.moveTools();
+}
 
-  // Mark all tools items that were originally here.
-  nativeToolsNotFound = true;
-  for (var i=0; i<origTools.snapshotLength; i++) {
-    var el=origTools.snapshotItem(i);
-    el.setAttribute('nativeTool', 'true');
-    if (nativeToolsNotFound) nativeToolsNotFound = false;
-  }
-  origTools=null;
-
-  // Move any non-marked tools that are already present.
-  moveAllTools();
-
-  // Move any non-marked tool that are added later.
-  toolsMenuPopup.addEventListener('DOMNodeInserted', moveAllTools, true);
-}, true);
-
-})();
+window.addEventListener('load', moreTools_startup, true);
+window.addEventListener('unload', moreTools_shutdown, true);
--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,1 +1,32 @@
 pref('extensions.{9a7a67d3-3048-47fb-acde-d0f7ae51f86a}.description', 'chrome://moretools/locale/overlay.properties');
+
+pref('extensions.moretools.showWhenEmpty', false);
+pref('extensions.moretools.extensionToolsToKeep', 'menuAltPlugins, menu_options');
+pref('extensions.moretools.nativeToolsToMove', '');
+
+// Firefox main window
+pref('extensions.moretools.nativeTools.firefox.browser', 'menu_search, browserToolsSeparator, menu_openDownloads, menu_openAddons, sync-setup, sync-syncnowitem, devToolsSeparator, webDeveloperMenu, menu_pageInfo, sanitizeSeparator, privateBrowsingItem, sanitizeItem, prefSep, menu_preferences');
+
+// Thunderbird main window
+pref('extensions.moretools.nativeTools.thunderbird.messenger', 'tasksMenuMail, addressBook, devToolsSeparator, menu_openSavedFilesWnd, addonsManager, activityManager, devToolsSeparator, filtersCmd, applyFilters, applyFiltersToSelection, tasksMenuAfterApplySeparator, runJunkControls, deleteJunk, tasksMenuAfterDeleteSeparator, menu_import, javascriptConsole, prefSep, menu_accountmgr, menu_preferences');
+
+// SeaMonkey mail window
+pref('extensions.moretools.nativeTools.seamonkey.messenger', 'menu_SearchMail, menu_SearchAddresses, tasksMenuAfterAddressesSeparator, menu_Filters, applyFilters, applyFiltersToSelection, tasksMenuAfterApplySeparator, runJunkControls, deleteJunk, tasksMenuAfterDeleteSeparator, menu_import, tasksDataman, menu_passwordManager, downloadmgr, addonsmgr, sync-setup, sync-syncnowitem, sep_switchprofile, cmd_switchprofile');
+
+// SeaMonkey browser
+pref('extensions.moretools.nativeTools.seamonkey.navigator', 'menu_searchWeb, menu_translate, menu_cookieManager, menu_imageManager, menu_popupManager, navBeginGlobalItems, sanitizeItem, tasksDataman, menu_passwordManager, downloadmgr, addonsmgr, sync-setup, sync-syncnowitem, sep_switchprofile, cmd_switchprofile');
+
+// Thunderbird mail composer
+pref('extensions.moretools.nativeTools.thunderbird.messengercompose', 'tasksMenuMail, tasksMenuAddressBook, prefSep, menu_preferences');
+
+// SeaMonkey mail composer
+pref('extensions.moretools.nativeTools.seamonkey.messengercompose', 'tasksDataman, menu_passwordManager, downloadmgr, addonsmgr, sync-setup, sync-syncnowitem, sep_switchprofile, cmd_switchprofile');
+
+// Thunderbird address book
+pref('extensions.moretools.nativeTools.thunderbird.addressbook', 'tasksMenuMail, prefSep, menu_preferences');
+
+// SeaMonkey address book
+pref('extensions.moretools.nativeTools.seamonkey.addressbook', 'menu_search_addresses, tasksDataman, menu_passwordManager, downloadmgr, addonsmgr, sync-setup, sync-syncnowitem, sep_switchprofile, cmd_switchprofile');
+
+// SeaMonkey HTML editor
+pref('extensions.moretools.nativeTools.seamonkey.editor', 'menu_validate, sep_validate, tasksDataman, menu_passwordManager, downloadmgr, addonsmgr, sync-setup, sync-syncnowitem, sep_switchprofile, cmd_switchprofile');
